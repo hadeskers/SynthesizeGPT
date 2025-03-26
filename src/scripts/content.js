577b372f72b01acef159daaa89db1d5569f0005e
hadesker <namgnuyen12041994@gmail.com>
2025-03-27 01:09:40 +0700

    [UPDATE] 1. Fix getAccessToken

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,3 +1,4 @@
+console.log('Synthesize: Load content.js');
 const s = document.createElement('script');
 s.src = chrome.runtime.getURL('src/scripts/inject.js');
 s.onload = function() {
--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -21,14 +21,27 @@ function downloadBlob(blob, filename) {
     window.URL.revokeObjectURL(url);
 }
 
+function isValidJWT(token) {
+    if (!token) {
+        return false;
+    }
+    const jwtRegex = /^[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+$/;
+    return jwtRegex.test(token);
+}
+
 function getAccessToken() {
-    return window.__NEXT_DATA__?.props?.pageProps?.session?.accessToken || window.__remixContext?.state?.loaderData?.root?.clientBootstrap?.session?.accessToken;
+    const accessToken =  window.__NEXT_DATA__?.props?.pageProps?.session?.accessToken
+        || window.__remixContext?.state?.loaderData?.root?.clientBootstrap?.session?.accessToken
+        || window.__reactRouterContext?.state?.loaderData?.root?.clientBootstrap?.session?.accessToken
+        || document.body.innerHTML.split('accessToken\\",\\"').pop().split('\\"').shift();
+    return isValidJWT(accessToken) && accessToken;
 }
 
 var _globalVoices = null;
 async function fetchVoices() {
     const accessToken = getAccessToken();
     if (!accessToken) {
+        console.error('No access token');
         return;
     }
     return fetch('https://chatgpt.com/backend-api/settings/voices', {
@@ -44,7 +57,7 @@ async function fetchVoices() {
             _globalVoices = data['voices'];
             return _globalVoices;
         })
-        .catch(err => console.log(err));
+        .catch(err => console.error('Error load voices from server', err));
 }
 
 const _audioCached = {};
@@ -95,60 +108,60 @@ function fetchAudio({message_id, text_content, selected_voice}, callback) {
 
 window.addDownload = () => {
     const conversations = document.querySelectorAll('.agent-turn div[data-message-author-role="assistant"]:not([inserted])');
-
     conversations.forEach(item => {
-        const btnPlay = item.parentNode.parentNode.querySelector('button[data-testid="voice-play-turn-action-button"]');
-        const parent = btnPlay.parentNode.parentNode;
-        if (parent) {
-            item.setAttribute('inserted', 'true');
-            const btnDownload = document.createElement('button');
-            btnDownload.setAttribute('class', 'rounded-full text-green-500 w-8 h-8 border hover:bg-token-main-surface-secondary ml-3 flex justify-center items-center border-green-500');
-            btnDownload.innerHTML = iconDownload;
-            btnDownload.onclick = () => {
-                const text_content = item.innerText;
-                const message_id = item.getAttribute('data-message-id');
-                btnDownload.innerHTML = loadingSpinner;
-                btnDownload.disabled = true;
-                const selected_voice = btnDownload.getAttribute('selected-voice');
-                fetchAudio({message_id, text_content, selected_voice}, () => {
-                    btnDownload.innerHTML = iconDownload;
-                    btnDownload.disabled = false;
-                });
-            }
-            parent.classList.add(...['justify-between', 'mb-3']);
-            const span = document.createElement('span');
-            span.setAttribute('class', 'flex gap-2');
-            span.append(btnDownload);
-
-            if(_globalVoices && _globalVoices.length) {
-                btnDownload.setAttribute('selected-voice', _globalVoices[0].voice);
-
-                const selectBox = document.createElement('select');
-                selectBox.onchange = () => {
-                    btnDownload.setAttribute('selected-voice', selectBox.value);
-                }
-                selectBox.setAttribute('title', 'Select voice to download');
-                selectBox.setAttribute('class', 'py-0 bg-gray-50 border border-gray-300 h-8 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500');
-
-                _globalVoices.forEach(item => {
-                    const option = document.createElement('option');
-                    option.setAttribute('value', item.voice);
-                    option.textContent = item.name + ' - ' + item.description;
-                    selectBox.append(option);
-                });
-
-                span.append(selectBox);
-            }
-
-            parent.append(span);
+        const articleElement = item.closest('article[data-testid^="conversation-turn"]');
+        if(!articleElement) {
+            return;
         }
+        const btnPlay = articleElement.querySelector('button[data-testid="voice-play-turn-action-button"]');
+        if(!btnPlay) {
+            return;
+        }
+        item.setAttribute('inserted', 'true');
+        const parent = btnPlay.parentNode.parentNode;
+        const btnDownload = document.createElement('button');
+        btnDownload.setAttribute('class', 'rounded-full text-green-500 w-8 h-8 border hover:bg-token-main-surface-secondary ml-3 flex justify-center items-center border-green-500');
+        btnDownload.innerHTML = iconDownload;
+        btnDownload.onclick = () => {
+            const text_content = item.innerText;
+            const message_id = item.getAttribute('data-message-id');
+            btnDownload.innerHTML = loadingSpinner;
+            btnDownload.disabled = true;
+            const selected_voice = btnDownload.getAttribute('selected-voice');
+            fetchAudio({message_id, text_content, selected_voice}, () => {
+                btnDownload.innerHTML = iconDownload;
+                btnDownload.disabled = false;
+            });
+        }
+        parent.classList.add(...['justify-between', 'mb-3']);
+        const span = document.createElement('span');
+        span.setAttribute('class', 'flex gap-2');
+        span.append(btnDownload);
+
+        if(_globalVoices && _globalVoices.length) {
+            btnDownload.setAttribute('selected-voice', _globalVoices[0].voice);
+
+            const selectBox = document.createElement('select');
+            selectBox.onchange = () => {
+                btnDownload.setAttribute('selected-voice', selectBox.value);
+            }
+            selectBox.setAttribute('title', 'Select voice to download');
+            selectBox.setAttribute('class', 'py-0 bg-gray-50 border border-gray-300 h-8 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500');
+
+            _globalVoices.forEach(item => {
+                const option = document.createElement('option');
+                option.setAttribute('value', item.voice);
+                option.textContent = item.name + ' - ' + item.description;
+                selectBox.append(option);
+            });
+            span.append(selectBox);
+        }
+        parent.append(span);
     });
 }
 
-window.addEventListener('load', function () {
-    if (!['chatgpt.com', 'chat.openai.com'].includes(window.location.hostname)) {
-        return;
-    }
+console.log('Synthesize: Load inject.js');
+if (['chatgpt.com', 'chat.openai.com'].includes(window.location.hostname)) {
     fetchVoices()
         .then(isSuccess => {
             if (isSuccess) {
@@ -158,5 +171,4 @@ window.addEventListener('load', function () {
                 }, 2000);
             }
         });
-});
-
+}
